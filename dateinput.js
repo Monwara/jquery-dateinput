1cc8b7e63d9ed7202f6da2179cbbcf5cd0898bd3
Branko Vukelic <branko@herdhound.com>
2012-03-29 11:35:42 +0200

    Bumped version number

--- dateinput.js
+++ dateinput.js
@@ -3,7 +3,7 @@
  * *jQuery plugin that provides calendar-based date entry widget*
  *
  * @author Monwara LLC / Branko Vukelic <branko@herdhound.com>
- * @version 0.0.3
+ * @version 0.0.4
  */
 
 (function(define) {
